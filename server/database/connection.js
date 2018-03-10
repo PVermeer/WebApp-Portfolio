11c1896640ac71184c37522a5c8fc6432c1ed3cf
Pim Vermeer <pim.vermeer@outlook.com>
2018-03-10 12:32:23 +0100

    Added timeout to initial MongoDb connect. Fixed mongoose unhandled promise rejection.

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,7 +2,25 @@ const mongoose = require('mongoose');
 
 const config = require('../config');
 
-mongoose.connect(config.mongoDb);
+const options = {
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+};
+
+/* eslint-disable no-console */
+
+const connectWithRetry = () => {
+  mongoose.connect(config.mongoDb, options).then(
+    () => { console.log('Mongoose default connection ready to use.'); },
+    (error) => {
+      console.log(`\nWhoops, cannot connect to MongoDb at server start:\n\n${error}\n\nWill try again in 5 seconds!\n`);
+      setTimeout(() => {
+        connectWithRetry();
+      }, 5000);
+    },
+  );
+};
+connectWithRetry();
 
 mongoose.connection.on('connected', () => {
   console.log('Mongoose default connection open');
@@ -22,5 +40,3 @@ process.on('SIGINT', () => {
     process.exit(0);
   });
 });
-
-// require();
