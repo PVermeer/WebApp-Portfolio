c548150ff7b7d8e10fb97e2c68fa0225190f0b30
Pim Vermeer <pim.vermeer@outlook.com>
2018-03-22 17:06:11 +0100

    Added sidenav interaction to user page

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 
 import { UserService } from '../_services/user.service';
+import { MatToggle, MatToggleExp, SidenavContent } from '../_models/sidenav.model';
+import { SidenavService } from '../_services/sidenav.service';
 
 @Component({
   selector: 'app-user',
@@ -8,15 +10,37 @@ import { UserService } from '../_services/user.service';
   styleUrls: ['./user.component.css'],
   providers: [UserService],
 })
-export class UserComponent {
+export class UserComponent implements OnInit, AfterViewInit {
+
+  // Sidenav config
+  private sidenavToggle: MatToggle = 'open';
+  private expansionToggle: MatToggleExp = 'open';
+  // Sidenav content
+  private sidenavContent: SidenavContent[] = [{
+    title: 'User page',
+    items: [
+      { label: 'User details', path: 'userDetails' },
+    ],
+  }];
 
   public logout() {
     this.userService.logout();
   }
 
   constructor(
+    private sidenavService: SidenavService,
     private userService: UserService,
   ) { }
 
+  ngOnInit() {
+    // Sidenav config
+    this.sidenavService.passSidenavContent(this.sidenavContent);
+    this.sidenavService.passExpansionToggle(this.expansionToggle);
+  }
+
+  ngAfterViewInit() {
+    // Sidenav config
+    this.sidenavService.passSidenavToggle(this.sidenavToggle);
+  }
 
 }
