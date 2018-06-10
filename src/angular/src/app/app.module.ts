3fcda4c4fad28317962b6adeab1e682ae7ddf77e
Pim Vermeer <pim.vermeer@outlook.com>
2018-06-10 20:33:32 +0200

    Clean up refactor user details

--- src/angular/src/app/app.module.ts
+++ src/angular/src/app/app.module.ts
@@ -1,26 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { UserComponent } from './pages/user/user.component';
-
-import { AppRoutingModule } from './app-routing.module';
-import { UsersModule } from './_modules/users/users.module';
-import { ContactModule } from './_modules/contact/contact.module';
-import { SharedModule } from './_modules/_shared/shared.module';
-import { ContentModule } from './_modules/content/content.module';
-
-import { ErrorInterceptorProvider } from './error.interceptor';
-import { SidenavService } from './sidenav/sidenav.service';
-import { AdminComponent } from './pages/user/admin/admin.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { PageResolver } from './pages/page-resolver.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ErrorInterceptorProvider } from './error.interceptor';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { ErrorComponent } from './pages/error/error.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageResolver } from './pages/page-resolver.service';
+import { UserComponent } from './pages/user/user.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { SidenavService } from './sidenav/sidenav.service';
+import { ContactModule } from './_modules/contact/contact.module';
+import { ContentModule } from './_modules/content/content.module';
+import { UsersModule } from './_modules/users/users.module';
+import { SharedModule } from './_modules/_shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -30,7 +26,6 @@ import { ErrorComponent } from './pages/error/error.component';
     AboutComponent,
     ContactComponent,
     UserComponent,
-    AdminComponent,
     ErrorComponent,
   ],
   imports: [
