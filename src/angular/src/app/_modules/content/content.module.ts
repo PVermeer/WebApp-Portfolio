e071e86735f1d9aa9e5e99b526c49d336196e82b
Pim Vermeer <pim.vermeer@outlook.com>
2018-06-22 12:02:20 +0200

    Added fa-icons

--- src/angular/src/app/_modules/content/content.module.ts
+++ src/angular/src/app/_modules/content/content.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { SharedModule } from '../_shared/shared.module';
-import { ContentService } from './content.service';
 import { ContentManagementComponent } from './content-management/content-management.component';
 import { NewPageComponent } from './content-management/new-page/new-page.component';
-
+import { ContentService } from './content.service';
+import { VisualModule } from '../_visual/visual.module';
 
 @NgModule({
   imports: [
     SharedModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    VisualModule
   ],
   declarations: [
     ContentManagementComponent,
