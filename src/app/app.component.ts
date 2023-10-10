1db0bbf73a028c628bb205aebc0d828cfe83dc31
Pim Vermeer <pim.vermeer@gmail.com>
2023-10-10 15:02:54 +0200

    feat(home-page): project app card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { LayoutComponent } from './layout/layout.component';
+import { SvgService } from './svg.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, LayoutComponent],
+  imports: [CommonModule, MatIconModule, LayoutComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(svg: SvgService) {
+    svg.loadSvgIcons();
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
